84798e4419529104f5bc9633b548499e2887a2d1
fadedrifleman-umla <145532040+fadedrifleman-umla@users.noreply.github.com>
2024-03-10 12:49:38 +0530

    Update route.js

--- route/route.js
+++ route/route.js
@@ -5,5 +5,6 @@ const router = express.Router();
 
 router.route("/incoming-call").post(baseController.incoming);
 router.route("/respond").post(baseController.response);
+router.route("/test").get(baseController.getChat);
 
 module.exports = router;
