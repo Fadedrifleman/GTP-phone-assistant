7f2213c6ef92f0a833eeb3149461e1a6aa5b2df7
fadedrifleman-umla <145532040+fadedrifleman-umla@users.noreply.github.com>
2024-03-20 18:16:15 +0530

    Update route.js

--- route/route.js
+++ route/route.js
@@ -6,5 +6,6 @@ const router = express.Router();
 router.route("/incoming-call").post(baseController.incoming);
 router.route("/respond").post(baseController.response);
 router.route("/conversation").get(baseController.getChat);
+router.route("/appointment").get(baseController.getAppointmentData);
 
 module.exports = router;
